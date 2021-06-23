5d1091e5ed42d137a6e88cd41633bb0ac8bcff85
KudahShambare <kudahshambare263@gmail.com>
2021-06-23 18:23:23 +0200

    CSS Added

--- src/Countries.js
+++ src/Countries.js
@@ -1,16 +1,26 @@
 import React from "react";
 
 function Countries(props) {
-  console.log(props.allCountryScores[1].scores[1].n);
   return (
     <div id="countriesReturn">
       <ul>
         {props.allCountryScores.map((country, index) => {
           return (
             <li key={index}>
-              <h1>{country.name}</h1>
-              {props.allCountryScores[index].scores.map((player) => {
-                return <h2><pre>{player.n}                { player.s}</pre> </h2>
+              <h1>High Scores: {country.name}</h1>
+              <hr />
+              {props.allCountryScores[index].scores.map((player, num) => {
+                return (
+                  <div>
+                    <h2 key={num}>
+                      {" "}
+                      <pre>
+                        {player.n} {player.s}
+                      </pre>{" "}
+                    </h2>
+                    <hr />
+                  </div>
+                );
               })}
             </li>
           );
